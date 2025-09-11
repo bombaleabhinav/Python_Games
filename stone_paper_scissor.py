b2771e5915a2d9a07db1f8f23d83ba9499aaede0
Abhinav Bombale <bombaleabhinav@gmail.com>
2025-09-11 15:21:08 +0530

    Done with while loop

--- stone_paper_scissor.py
+++ stone_paper_scissor.py
@@ -3,13 +3,18 @@ import time
 
 
 game_elements=["Stone","Paper","Scissor"]
-i=0
+i=1
 
-while i ==0:
+while i ==1:
     print("1:Stone \n2:Paper \n3:Scissor")
     user_choice = int(input("Enter your choice: "))
 
     computer_choice = r.randint(1,3)
+
+    print("Thinking ")
+    time.sleep(0.8)
+
+    print("Computer selected",game_elements[computer_choice-1])
 
     if computer_choice == user_choice:
         print("This is a tie")
@@ -19,4 +24,9 @@ while i ==0:
 
     else:
         print("You win")
+
+    i =int(input("Type 1 to play again: "))
+
+
+
 
