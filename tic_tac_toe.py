55ea0994c1ec75e421cbdb7cdac89e26d9790768
Abhinav Bombale <bombaleabhinav@gmail.com>
2025-09-13 09:20:04 +0530

    Added logic to select possitions

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,23 +6,32 @@ row2 = [4,5,6]
 row3 = [7,8,9]
 
 print ("welcome!")
-print(row1 /nrow2 /nrow3)
+print("\n",row1,"\n",row2,"\n",row3)
 user_choice = int(input("Where do you want to place X: "))
+computer_choice= r.randint(1,9)
+    
 
-def allot(input1,symbol):
-    if input1 in row1:
-        row1[user_choice-1]= symbol
-    elif input1 in row3:
-        row2[user_choice-4] = symbol
-    else:
-        row3[user_choice-7] = symbol
-
-while i in range(5):
+for i in range(5):
     user_choice = int(input("Where do you want to place X: "))
-    allot(user_choice,"O")
-    print("Computer Thinking")
-    computer_choice= r.randint(1,9)
-    allot(computer_choice,"X")
+
+    if user_choice in row1:
+      row1[user_choice-1]= "O"
+    elif input1 in row3:
+      row2[user_choice-4] = "O"
+    else:
+     row3[user_choice-7] = "O"
+
+     print("Computer Thinking")
+     
+
+    if computer_choice in row1:
+        row1[computer_choice-1]= "O"
+    elif input1 in row3:
+        row2[computer_choice-4] = "O"
+    else:
+        row3[computer_choice-7] = "O"
+
+    print(row1,"\n",row2,"\n",row3)
 
 
 
